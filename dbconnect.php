d5ac6182fb731cb07e198196ad9cf057ceee842e
rishigehlot19 <91007523+rishigehlot19@users.noreply.github.com>
2021-09-19 17:24:54 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,1 +1,13 @@
-
+<?php
+$servername="localhost";
+$username="root";
+$password= "";
+$database="digitalclassroom";
+$conn = mysqli_connect($servername , $username , $password, $database);
+if(!$conn){
+    // echo "connection was successful<br>";
+// }
+// else{
+    die("sorry we failed to connect:". mysqli_connect_error());
+}
+?>
